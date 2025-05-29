5768632d6657de67db1be98ac7ed7e8ef47ef3c0
Erika muñoz <er.munozr@duocuc.cl>
2025-05-28 23:03:28 -0400

    creando formulario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  usuario: string = '';
+  nombre: string = '';
+  apellido: string = '';
+  nivelEducacion: string = '';
+  fechaNacimiento: string = '';
 
-  constructor() {}
+  constructor(private router: Router) {
+    const nav = this.router.getCurrentNavigation();
+    if (nav?.extras.state && nav.extras.state['usuario']) {
+      this.usuario = nav.extras.state['usuario'];
+    }
+  }
 
+  mostrar() {
+    alert(
+      `Nombre: ${this.nombre}\nApellido: ${this.apellido}\nEducación: ${this.nivelEducacion}\nFecha Nac: ${this.fechaNacimiento}`
+    );
+  }
+
+  limpiar() {
+    this.nombre = '';
+    this.apellido = '';
+    this.nivelEducacion = '';
+    this.fechaNacimiento = '';
+  }
 }
