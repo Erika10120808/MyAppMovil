54c9f75aa47c795114e45ad082ab337a6c0a8849
Erika muñoz <er.munozr@duocuc.cl>
2025-06-01 22:22:48 -0400

    Revert "ingresando botones"
    This reverts commit 96d4cc20560305a71021cb69901d402632efffa5.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -29,44 +30,38 @@ export class HomePage implements OnInit {
   usuario: string = '';
   nombre: string = '';
   apellido: string = '';
-  correo: string = '';
-  password: string = '';
   nivelEducacion: string = '';
   fechaNacimiento: string = '';
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       if (params['usuario']) {
         this.usuario = params['usuario'];
-      } else {
-        alert('No se ha iniciado sesión correctamente. Serás redirigido al Login.');
-        this.router.navigate(['/login']);
       }
     });
   }
 
   mostrar() {
     alert(
-      `Nombre: ${this.nombre}\nApellido: ${this.apellido}\nCorreo: ${this.correo}\nContraseña: ${this.password}\nEducación: ${this.nivelEducacion}\nFecha de Nacimiento: ${this.fechaNacimiento}`
+      `Nombre: ${this.nombre}\nApellido: ${this.apellido}\nEducación: ${this.nivelEducacion}\nFecha de Nacimiento: ${this.fechaNacimiento}`
     );
   }
 
   limpiar() {
     this.nombre = '';
     this.apellido = '';
-    this.correo = '';
-    this.password = '';
     this.nivelEducacion = '';
     this.fechaNacimiento = '';
 
+    // Aplica la animación de izquierda a derecha
     const nombreInput = document.querySelector('#nombreInput');
     const apellidoInput = document.querySelector('#apellidoInput');
 
     if (nombreInput) {
       nombreInput.classList.remove('animate-input');
-      void (nombreInput as HTMLElement).offsetWidth;
+      void (nombreInput as HTMLElement).offsetWidth; // Forzar reinicio
       nombreInput.classList.add('animate-input');
     }
     if (apellidoInput) {
@@ -74,13 +69,5 @@ export class HomePage implements OnInit {
       void (apellidoInput as HTMLElement).offsetWidth;
       apellidoInput.classList.add('animate-input');
     }
-  }
-
-  volverLogin() {
-    this.router.navigate(['/login']);
-  }
-
-  irARegistrar() {
-    this.router.navigate(['/login']);
   }
 }
