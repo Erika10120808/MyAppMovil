ff221a0a46ef3bb5f285a660b4a3ad335977e1f5
Erika muñoz <er.munozr@duocuc.cl>
2025-06-21 23:00:09 -0400

    reconfigurando

--- src/app/configuracion/configuracion.page.ts
+++ src/app/configuracion/configuracion.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonHeader } from "@ionic/angular/standalone";
+
 
 @Component({
   selector: 'app-configuracion',
