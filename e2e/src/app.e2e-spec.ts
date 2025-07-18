0d0bf2ec24766294ec296eb69856055a95ce1b19
Erika muñoz <er.munozr@duocuc.cl>
2025-07-17 21:27:18 -0400

    e2e

--- e2e/src/app.e2e-spec.ts
+++
@@ -1,9 +0,0 @@
-import { browser } from 'protractor';
-
-describe('Madres Conectadas App', () => {
-  it('debería tener un título', async () => {
-    await browser.get('http://localhost:8100');
-    const title = await browser.getTitle();
-    expect(title).toEqual('MadresConectadas2.0');
-  });
-});
